67beaef8a3073c7f5e3a4a32450aa5a9e1afe637
Vu Tam R <vutam311291@gmail.com>
2023-08-30 09:37:01 +0700

    add Styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
 
 const App = () => {
+	let name = 'Alphonse';
+	let number = 2021;
+	let obj = { name: 'David', channel: 'Funny' };
+	let link = 'https://www.youtube.com/watch?v=Lh0gDi-sQXg&t=462s';
+
 	return (
 		<div className='App'>
+			<Nav />
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Hello world from ReactJS</p>
+				<p>Hello world from ReactJS and {name}!</p>
+				<p>{JSON.stringify(obj)}</p>
+				<a href={link} target='_blank'>
+					Vist my channel
+				</a>
 			</header>
 		</div>
 	);
