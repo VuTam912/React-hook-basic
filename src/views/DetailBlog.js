1ec9c0e12f04e8fd694e985d1da8a4c17d18719b
Vu Tam R <vutam311291@gmail.com>
2023-08-31 11:36:35 +0700

    use useParams and useHistory

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,12 +1,22 @@
 // su dung hook voi react-router-dom
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 const DetailBlog = () => {
 	// lay tham so tren URL ..id=
 	let { id } = useParams();
+	// back page
+	let history = useHistory();
+
+	const handleBackData = () => {
+		// history.goBack();
+		history.push('/blog');
+	};
 
 	return (
 		<div>
+			<div>
+				<button onClick={handleBackData}>&lt;-- Back</button>
+			</div>
 			<h1>Hello detail blog with id = {id} </h1>
 		</div>
 	);
