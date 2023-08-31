5b1ed8e91aad426c0de3562055d286b646047e5d
Vu Tam R <vutam311291@gmail.com>
2023-08-31 11:47:25 +0700

    reuse custom hook

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,11 +1,21 @@
 // su dung hook voi react-router-dom
 import { useParams, useHistory } from 'react-router-dom';
+import useFetch from '../customize/fetch';
+import './Blog.scss';
 
 const DetailBlog = () => {
 	// lay tham so tren URL ..id=
 	let { id } = useParams();
 	// back page
 	let history = useHistory();
+
+	//Fetch 1 data
+	const {
+		data: dataBlogData,
+		isLoading,
+		isError,
+	} = useFetch(`
+    https://jsonplaceholder.typicode.com/posts/${id}`);
 
 	const handleBackData = () => {
 		// history.goBack();
@@ -17,7 +27,17 @@ const DetailBlog = () => {
 			<div>
 				<button onClick={handleBackData}>&lt;-- Back</button>
 			</div>
-			<h1>Hello detail blog with id = {id} </h1>
+			<div className='blogs-container'>
+				<div className='single-blog'>
+					{isLoading === false && dataBlogData && (
+						<>
+							<div className='title'>Blog ID: {dataBlogData.title}</div>
+							<div className='content'>{dataBlogData.body}</div>
+						</>
+					)}
+					{isLoading === true && <div>Loading data...</div>}
+				</div>
+			</div>
 		</div>
 	);
 };
